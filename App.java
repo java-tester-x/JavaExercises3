29091d9f6706257adc5f38c4eea38ccc5cb88923
java-tester-x <iohonko@gmail.com>
2014-06-06 19:34:06 +0300

    First commit

---
+++ App.java
@@ -0,0 +1,38 @@
+import src.*;
+
+public class JavaSamples {
+
+    public static void main(String[] args) {
+        System.out.println("\n******** TASK01 **************");
+        // Task01.main(args);
+        
+        System.out.println("\n******** TASK02 **************");
+        // Task02.main(args);
+
+        System.out.println("\n******** TASK03 **************");
+        // Task03.main(args);
+
+        System.out.println("\n******** TASK04 **************");
+        //Task04.main(args);
+        
+        System.out.println("\n******** TASK05 **************");
+        // Task05.main(args);
+
+        System.out.println("\n******** TASK06 **************");
+        // Task06.main(args);
+
+        System.out.println("\n******** TASK07 **************");
+        // Task07.main(args);
+
+        System.out.println("\n******** TASK08 **************");
+        // Task08.main(args);
+
+        System.out.println("\n******** TASK09 **************");
+        // Task09.main(args);
+
+        System.out.println("\n******** TASK10 **************");
+        Task10.main(args);
+    }
+
+}
+
---
+++ runApp.bat
@@ -0,0 +1,3 @@
+javac -Xlint:unchecked -cp .;lib/* -d bin App.java
+java  -cp .;lib/*;bin/ App
+pause
